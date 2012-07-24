15307bb424dd943af0abdf02adfddbbd65cb3bc1
Raynos <raynos2@gmail.com>
2012-07-24 11:20:45 -0700

    .concat flattens arrays passed in

--- index.js
+++ index.js
@@ -1,14 +1,18 @@
 exports = module.exports = ap;
 function ap (args, fn) {
     return function () {
-        return fn.apply(this, args.concat.apply(args, arguments));
+        var rest = [].slice.call(arguments)
+        args.push.apply(args, rest)
+        return fn.apply(this, args);
     };
 }
 
 exports.pa = pa;
 function pa (args, fn) {
     return function () {
-        return fn.apply(this, [].slice.call(arguments).concat(args));
+        var rest = [].slice.call(arguments)
+        rest.push.apply(rest, args)
+        return fn.apply(this, rest);
     };
 }
 
